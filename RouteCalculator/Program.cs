23c3f0329d586d657a590d2bb0fe773c24fbd6d6
Marcel Valdez <marcel.valdez@live.com>
2012-07-09 21:57:05 -0500

    Added FxCop rules project. Modified project files.

--- RouteCalculator/Program.cs
+++ RouteCalculator/Program.cs
@@ -1,14 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace RouteCalculator
+﻿namespace RouteCalculator
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    /// <summary>
+    /// The main program in charge of execution.
+    /// </summary>
     class Program
     {
+        /// <summary>
+        /// Main entry point for the application
+        /// </summary>
+        /// <param name="args">The args.</param>
         static void Main(string[] args)
         {
+
         }
     }
 }
