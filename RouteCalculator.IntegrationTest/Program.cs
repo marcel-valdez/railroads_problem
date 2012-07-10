147251128a860aff1f217932b290dd4eb7b205e0
Marcel Valdez <marcel.valdez@live.com>
2012-07-09 21:39:23 -0500

    Added nunit to tools

--- RouteCalculator.IntegrationTest/Program.cs
+++ RouteCalculator.IntegrationTest/Program.cs
@@ -1,14 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace RouteCalculator.IntegrationTest
+﻿namespace RouteCalculator.IntegrationTest
 {
-    class Program
+    using System;
+    using System.Collections.Generic;
+    using System.Reflection;
+    using NUnit.ConsoleRunner;
+
+    /// <summary>
+    /// Automated unit testing program for Windows Calculator
+    /// </summary>
+    public sealed class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// Prevents a default instance of the <see cref="Program"/> class from being created.
+        /// </summary>
+        private Program()
         {
+        }
+
+        /// <summary>
+        /// Main entry point for this program.
+        /// </summary>
+        /// <param name="args">The arguments received from the console.</param>
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            var argList = new List<string>();
+            argList.Add(Assembly.GetExecutingAssembly().Location);
+            argList.AddRange(args);
+
+            int returnCode = Runner.Main(argList.ToArray());
+
+            if (returnCode != 0)
+            {
+                Console.Beep();
+            }
         }
     }
 }
